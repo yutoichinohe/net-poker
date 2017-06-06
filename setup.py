5f8e8dc91b679c635296977cfe656f003b46ea70
Yuto Ichinohe <yutoichinohe@gmail.com>
2017-06-06 16:20:57 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+Emoji = False
+
+ActionHistoryLength = 30
+
+StageDurationSec = 3*60            # sec
+
+BlindGen = 'Default'
+#BlindGen='PokerStarsSNG'
+#BlindGen='FullTiltPokerSNG'
+#BlindGen='AJPC2014'
+#BlindGen='WSOP2016'
