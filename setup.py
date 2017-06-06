47ba632f7eb64efdae75efbecb396f2305ebe79e
Yuto Ichinohe <yutoichinohe@gmail.com>
2017-06-06 17:28:21 +0200

    spacing

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ ActionHistoryLength = 30
 StageDurationSec = 3*60            # sec
 
 BlindGen = 'Default'
-#BlindGen='PokerStarsSNG'
-#BlindGen='FullTiltPokerSNG'
-#BlindGen='AJPC2014'
-#BlindGen='WSOP2016'
+#BlindGen = 'PokerStarsSNG'
+#BlindGen = 'FullTiltPokerSNG'
+#BlindGen = 'AJPC2014'
+#BlindGen = 'WSOP2016'
